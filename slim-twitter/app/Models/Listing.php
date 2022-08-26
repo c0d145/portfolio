0158cd6748f72bad765632e5d4bebd3469157e7f
c0d145 <tschill@techfak.de>
2022-08-26 17:53:52 +0200

    ADD slim-twitter: First migrations

--- slim-twitter/app/Models/Listing.php
+++ slim-twitter/app/Models/Listing.php
@@ -1,27 +1,11 @@
 <?php
-    namespace App\Models;
-    class Listing{
-        public static function all() {
-            return [
-                [
-                    'id' => 1,
-                    'title' => 'Listing One',
-                    'description' => 'This is listing number one.'
-                ], 
-                [
-                    'id' => 2,
-                    'title' => 'Listing One',
-                    'description' => 'This is listing number two.'
-                ]
-                ];
-        }
-        public static function find($id) {
-            $listings = self::all();
-            foreach ($listings as $listing) {
-                if($listing['id'] == $id) {
-                    return $listing;
-                }
-            }
-        }
-    }
- 
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory;
+}
