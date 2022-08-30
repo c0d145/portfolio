e3eeb0b202c69d0c184a4f6e12952df8357bcba9
c0d145 <tschill@techfak.de>
2022-08-30 02:08:22 +0200

    ADD slim-twitter: Own User Creation + Login

--- slim-twitter/routes/web.php
+++ slim-twitter/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
-use App\Http\Controllers\ListingController;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Http\Request;
 use App\Models\Listing;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ListingController;
 
 /*
 |--------------------------------------------------------------------------
@@ -48,19 +49,19 @@ Route::get('/listings', [ListingController::class, 'index']);
 //     ]);
 // });
 
-// Show Create Form
+// Show create form
 Route::get('/listings/create', [ListingController::class, 'create']);
 
-// Store Listing Data
+// Store listing data
 Route::post('/listings', [ListingController::class, 'store']);
 
-// Show Edit Form
+// Show edit form
 Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
 
-// Update Listing
+// Update listing
 Route::put('/listings/{listing}', [ListingController::class, 'update']);
 
-// Delete Listing
+// Delete listing
 Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 
 // Show single listing with route model binding. -> Automatic 404 for non existing IDs.
@@ -68,8 +69,23 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 // interpreted as ID checks.
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// Show register / create Form
+Route::get('/register', [UserController::class, 'create']);
 
-require __DIR__ . '/auth.php';
+// Create new user
+Route::post('/users', [UserController::class, 'store']);
+
+// Log user Out
+Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
+
+// Show login form
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+
+// Login user
+Route::post('/users/authenticate', [UserController::class, 'authenticate']);
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth'])->name('dashboard');
+
+// require __DIR__ . '/auth.php';
